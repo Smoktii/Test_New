226263f652348446c10e2b0980ce22dd3d41c80d
Smoktii <smoktii@gmail.com>
2020-09-14 15:38:35 +0300

    commmi1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Donnu!!!");
+            Console.WriteLine("Hello Donnu!!--!!");
         }
     }
 }
